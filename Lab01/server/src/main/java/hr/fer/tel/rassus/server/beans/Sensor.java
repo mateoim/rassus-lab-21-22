4d335233413df15bc8a2aee3ad651edc1fb23702
Mateo <mateoim@me.com>
2021-10-27 17:29:43 +0200

    Add Sensor implementation

--- Lab01/server/src/main/java/hr/fer/tel/rassus/server/beans/Sensor.java
+++ Lab01/server/src/main/java/hr/fer/tel/rassus/server/beans/Sensor.java
@@ -1,5 +1,81 @@
 package hr.fer.tel.rassus.server.beans;
 
+import org.springframework.lang.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Entity
 public class Sensor {
-  //  TODO
+
+    @Id
+    @GeneratedValue
+    @NonNull
+    private long id;
+
+    @NonNull
+    private double latitude;
+
+    @NonNull
+    private double longitude;
+
+    @NonNull
+    private String ip;
+
+    @NonNull
+    private int port;
+
+    @OneToMany(mappedBy = "sensor")
+    private Set<Reading> readings;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public Set<Reading> getReadings() {
+        return readings;
+    }
+
+    public void setReadings(Set<Reading> readings) {
+        this.readings = readings;
+    }
 }
