dcba82694b27ca38d6116284e4426c9c7f6c8e36
Mateo <mateoim@me.com>
2021-10-27 17:30:44 +0200

    Add Reading implementation

--- Lab01/server/src/main/java/hr/fer/tel/rassus/server/beans/Reading.java
+++ Lab01/server/src/main/java/hr/fer/tel/rassus/server/beans/Reading.java
@@ -1,7 +1,97 @@
 package hr.fer.tel.rassus.server.beans;
 
-public class Reading {
-  //  TODO
+import org.springframework.lang.NonNull;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Reading {
+
+    @Id
+    @GeneratedValue
+    @NonNull
+    private long id;
+
+    private double temperature;
+
+    private double pressure;
+
+    private double humidity;
+
+    private double co;
+
+    private double no2;
+
+    private double so2;
+
+    @ManyToOne
+    private Sensor sensor;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getCo() {
+        return co;
+    }
+
+    public void setCo(double co) {
+        this.co = co;
+    }
+
+    public double getNo2() {
+        return no2;
+    }
+
+    public void setNo2(double no2) {
+        this.no2 = no2;
+    }
+
+    public double getSo2() {
+        return so2;
+    }
+
+    public void setSo2(double so2) {
+        this.so2 = so2;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
 }
 
