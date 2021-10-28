28bb3fab2d525cc806cab6a570e6af901445e605
mateoim <mateoim@me.com>
2021-10-28 11:46:03 +0200

    Add Sensor registration and list

--- Lab01/server/src/main/java/hr/fer/tel/rassus/server/controllers/SensorsController.java
+++ Lab01/server/src/main/java/hr/fer/tel/rassus/server/controllers/SensorsController.java
@@ -1,11 +1,44 @@
 package hr.fer.tel.rassus.server.controllers;
 
+import hr.fer.tel.rassus.server.beans.Sensor;
+import hr.fer.tel.rassus.server.services.SensorRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/sensors")
 public class SensorsController {
 
-    //  TODO 4.1  Registracija
+    private final SensorRepository sensorRepository;
 
-    //  TODO 4.4  Popis senzora
+    public SensorsController(SensorRepository sensorRepository) {
+        this.sensorRepository = sensorRepository;
+    }
+
+    @PutMapping
+    public ResponseEntity<Sensor> registerSensor(@RequestBody Sensor sensor) {
+        sensorRepository.save(sensor);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(sensor.getId()).toUri();
+
+        return ResponseEntity.created(location).body(sensor);
+    }
+
+    @GetMapping
+    public List<Sensor> getSensors() {
+        return sensorRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Sensor getSensor(@PathVariable Long id) {
+        return sensorRepository.findById(id).orElseThrow();
+    }
 
     //  TODO 4.2  Najbliži susjed
 
