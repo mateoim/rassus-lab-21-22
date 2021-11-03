6e6ca6942d9b3907c4714c5f1547decd980cc754
Mateo <mateoim@me.com>
2021-11-03 10:22:14 +0100

    Implement requestReading method

--- Lab01/client/src/main/java/hr/fer/tel/rassus/client/grpc/ReadingService.java
+++ Lab01/client/src/main/java/hr/fer/tel/rassus/client/grpc/ReadingService.java
@@ -1,5 +1,7 @@
 package hr.fer.tel.rassus.client.grpc;
 
+import hr.fer.tel.rassus.client.Client;
+import hr.fer.tel.rassus.client.model.Reading;
 import hr.fer.tel.rassus.examples.ReadingGrpc;
 import hr.fer.tel.rassus.examples.ReadingRequest;
 import hr.fer.tel.rassus.examples.ReadingResponse;
@@ -11,8 +13,28 @@ public class ReadingService extends ReadingGrpc.ReadingImplBase {
 
     private static final Logger logger = Logger.getLogger(ReadingService.class.getName());
 
+    private final Client client;
+
+    public ReadingService(Client client) {
+        this.client = client;
+    }
+
     @Override
     public void requestReading(ReadingRequest request, StreamObserver<ReadingResponse> responseObserver) {
-        super.requestReading(request, responseObserver);
+        logger.info("New request form " + request.getId());
+
+        Reading reading = client.getLatestReading();
+        ReadingResponse response =ReadingResponse.newBuilder()
+                .setTemperature(reading.getTemperature())
+                .setPressure(reading.getPressure())
+                .setHumidity(reading.getHumidity())
+                .setCo(reading.getCo())
+                .setNo2(reading.getNo2())
+                .setSo2(reading.getNo2())
+                .build();
+
+        responseObserver.onNext(response);
+        logger.info("Response sent");
+        responseObserver.onCompleted();
     }
 }
