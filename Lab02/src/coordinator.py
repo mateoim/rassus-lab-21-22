4da5f06645432603f9e071e0a270bdfe20445b77
Mateo <mateoim@me.com>
2021-12-05 12:47:56 +0100

    Add coordinator

---
+++ Lab02/src/coordinator.py
@@ -0,0 +1,16 @@
+import kafka
+
+
+if __name__ == '__main__':
+    coordinator = kafka.KafkaProducer(bootstrap_servers=['localhost:9092'])
+
+    line = ''
+    while line.lower() != 'start':
+        line = input('Type "start" to send Start message:\n')
+
+    coordinator.send('Command', b'Start')
+
+    while line.lower() != 'stop':
+        line = input('Type "stop" to send Stop message:\n')
+
+    coordinator.send('Command', b'Stop')
