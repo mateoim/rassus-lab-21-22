8f12fa037309bc68ee0eb532f617bab3721a70a3
mateoim <mateoim@me.com>
2021-12-09 10:47:38 +0100

    Change data types to float

--- Lab02/src/reading.py
+++ Lab02/src/reading.py
@@ -1,11 +1,11 @@
 class Reading:
-    def __init__(self, temperature, pressure, humidity, co=None, no2=None, so2=None):
-        self.temperature = temperature
-        self.pressure = pressure
-        self.humidity = humidity
-        self.co = co
-        self.no2 = no2
-        self.so2 = so2
+    def __init__(self, temperature, pressure, humidity, co='', no2='', so2=''):
+        self.temperature = float(temperature)
+        self.pressure = float(pressure)
+        self.humidity = float(humidity)
+        self.co = float(co) if co != '' else 0
+        self.no2 = float(no2) if no2 != '' else 0
+        self.so2 = float(so2) if so2 != '' else 0
 
     def __repr__(self) -> str:
         return f'{self.temperature},{self.pressure},{self.humidity},{self.co},{self.no2},{self.so2}'
