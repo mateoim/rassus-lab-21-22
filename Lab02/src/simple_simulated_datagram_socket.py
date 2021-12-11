a536d626f4cceaa799076dc5953b8b780530caa6
mateoim <mateoim@me.com>
2021-12-11 12:15:10 +0100

    Remove port argument from constructor and add it to send_packet

--- Lab02/src/simple_simulated_datagram_socket.py
+++ Lab02/src/simple_simulated_datagram_socket.py
@@ -5,20 +5,19 @@ import time
 
 
 class SimpleSimulatedDatagramSocket(socket.socket):
-    def __init__(self, port: int, loss_rate: float, average_delay: int):
+    def __init__(self, loss_rate: float, average_delay: int):
         super().__init__(socket.AF_INET, socket.SOCK_DGRAM)
 
-        self.address = ('localhost', port)
         self.loss_rate = loss_rate
         self.average_delay = average_delay
 
-        super().settimeout(0)
+        super().settimeout(4 * average_delay)
 
-    def send_packet(self, packet):
+    def send_packet(self, packet, address):
         if random.uniform(0, 1) >= self.loss_rate:
             sleep_time = 2 * self.average_delay * random.uniform(0, 1)
-            threading.Thread(target=self._send_packet, args=(packet, sleep_time)).start()
+            threading.Thread(target=self._send_packet, args=(packet, address, sleep_time)).start()
 
-    def _send_packet(self, packet, sleep_time):
+    def _send_packet(self, packet, address, sleep_time):
         time.sleep(sleep_time / 1000)
-        super().sendto(packet, self.address)
+        super().sendto(packet, address)
