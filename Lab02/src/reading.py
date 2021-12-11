e0df060ec03fe8e5fab8bf6e08f9186c8f8c1a7e
mateoim <mateoim@me.com>
2021-12-11 11:58:34 +0100

    Remove unused readings and add reading id

--- Lab02/src/reading.py
+++ Lab02/src/reading.py
@@ -1,11 +1,7 @@
 class Reading:
-    def __init__(self, temperature, pressure, humidity, co='', no2='', so2=''):
-        self.temperature = float(temperature)
-        self.pressure = float(pressure)
-        self.humidity = float(humidity)
+    def __init__(self, reading_id, co):
+        self.id = reading_id
         self.co = float(co) if co != '' else 0
-        self.no2 = float(no2) if no2 != '' else 0
-        self.so2 = float(so2) if so2 != '' else 0
 
     def __repr__(self) -> str:
-        return f'{self.temperature},{self.pressure},{self.humidity},{self.co},{self.no2},{self.so2}'
+        return f'(id: {self.id}, CO: {self.co})'
